c8fb1d16c2e24e96e5763d2f3890f1dff7da1cb5
Sergey <hojalata@mail.ru>
2022-08-30 15:39:22 +0300

    Выполнил задание 47

---
+++ Task_1/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 47. Задайте двумерный массив 
+// размером m×n, заполненный случайными 
+// вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+double[,] GetArray(double m, double n)
+{
+    double[,] matrix = new double[3, 4];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().NextDouble() * 30 - 10;
+        }
+    }
+    return matrix;
+}
+
+double[,] result = GetArray(3, 4);
+void PrintArray(double[,] inputMatrix)
+{
+    for (int i = 0; i < inputMatrix.GetLength(0); i++)
+    {
+        for (int m = 0; m < inputMatrix.GetLength(1); m++)
+        {
+            Console.Write(inputMatrix[i, m] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+PrintArray(result);
